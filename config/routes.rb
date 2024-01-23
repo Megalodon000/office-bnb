d1b9a6b2ed24af1e301d46ac606cce4b9021c718
ali mercade <alimercad@gmail.com>
2024-01-23 14:55:47 +0100

    office bnb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    resources :bookings, only: [:index, :show, :new, :create, :destroy]
+    resources :offices, only: [:show, :new, :create, :destroy]
+  end
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -10,6 +13,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :offices
+  resources :offices, only: [:index, :new, :create, :destroy]
   
 end
